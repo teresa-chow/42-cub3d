21c2ae61e9addbeae028e4707cb42a21fb1192a8
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-07-03 19:11:28 +0100

    chore: structural comments

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 16:41:55 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/07/01 12:23:20 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/07/03 18:07:42 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ int	main(int argc, char **argv)
 {
 	t_data	img; //mlx info
 
-
 	check_input(argc, argv[1]);
 	ft_bzero(&img, sizeof(img));
 	//validate map
 	new_img(&img);
-	//new img + rendering loop
+	//rendering loop
 	//free map
 	return (0);
 }
--- src/rendering/raycaster.c
+++ src/rendering/raycaster.c
@@ -6,11 +6,16 @@
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 14:31:02 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/07/01 12:25:23 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/07/03 18:25:08 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
+
+/* Digital Differential Analysis (DDA) */
+//dda_algorithm
+
+//cal_ray_wall_dist
 
 /*
 int	raycaster(int argc, char **argv)
