ce85e380b5eb5647092e935df22f3c3ed6c164ba
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-07-11 19:51:54 +0100

    chore: adapting dda to draw minimap (wip)

--- src/renderer/minimap.c
+++ src/renderer/minimap.c
@@ -6,30 +6,49 @@
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 18:12:30 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/07/11 18:48:06 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/07/11 19:51:14 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/render.h"
 
-/*void    draw_minimap(t_raycaster *rc)
+/*static void calc_minimap_spacing(t_raycaster *rc, float *spacing);
+
+void    draw_minimap(t_raycaster *rc)
 {
-	//check if map width or height is bigger
-	//calculate them proportionally to WIN_W and WIN_H (set max width or height)
-	//consider that value to know dimension of grid
-	//run through map while drawing it
-    line_drawing_dda(rc->img)
-    rc->world->map_len //contains the number of cells, no the spacing between them,
-	that has to be calculated
-    rc->world->map_wid //same
+	float	spacing;
+	int	x;
+	int	y;
+
+	calc_minimap_spacing(rc, &spacing);
+	y = 0;
+	while (y < rc->world->map_len)
+	{
+		x = 0;
+		while (x < rc->world->map_wid - 1)
+		{
+			line_drawing_dda(rc, y, x, spacing);
+			x++;
+		}
+		line_drawing_dda(rc, y, x, spacing); // ?
+		y++;
+	}
 }
 
-void	line_drawing_dda(t_data *img, //point and next_point)
+static void calc_minimap_spacing(t_raycaster *rc, float *spacing)
+{
+	if (rc->world->map_len > rc->world->map_wid)
+		spacing = (WIN_H / 4) / rc->world->map_len;
+	else
+		spacing = (WIN_W / 5) / rc->world->map_wid;
+}
+
+void	line_drawing_dda(t_raycaster *rc, int y, int x, float spacing)
 {
 	int			i;
 	t_dda		dda;
 
-	dda.dx = next_point->x - point->x;
+	dda.dx = (x + 1) * spacing - point->x;
 	dda.dy = next_point->y - point->y;
 	if (fabsf(dda.dx) > fabsf(dda.dy))
 		dda.step = fabsf(dda.dx);
