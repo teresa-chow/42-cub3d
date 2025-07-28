914dabad6b21b51b642edd890b6fb9e8ee7c7383
Teresa Chow <teresa-chow@users.noreply.github.com>
2025-07-28 13:05:28 +0100

    chore: renamed test maps

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 16:41:55 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/07/28 11:48:32 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/07/28 12:55:09 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int	main(int argc, char **argv)
 
 	check_input(argc, argv[1]);
 	init_vars(&img, &world, &rc);
-	//validate_map(argv[1], &world);
+	validate_map(argv[1], &world);
 	//fill t_world world; create and fill t_camera *cam
-	launch_render_engine(&img, &world, &rc);
+	//launch_render_engine(&img, &world, &rc);
 	//free world
 	return (0);
 }
