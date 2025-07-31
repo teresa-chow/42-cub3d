e3559771239a4585a7d1bcd60087e56785a0de40
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-07-31 12:06:32 +0100

    feat: added new test maps

--- assets/test_maps.sh
+++ assets/test_maps.sh
@@ -5,7 +5,9 @@
 # =========================================================================== #
 
 PROGRAM=./cub3D
-MAP_DIRS=("assets/maps/tex")
+MAP_DIRS=("assets/maps/invalid/color" "assets/maps/invalid/content"
+    "assets/maps/invalid/tex" "assets/maps/valid/color"
+    "assets/maps/valid/content" "assets/maps/valid/tex")
 
 # -----------------------------------------------------------------------------
 # UTILS: ANSI COLOR SEQUENCES
