15cbd2d752a0df1d4d137b8c539cc65e6f756e4f
Teresa Chow <teresa-chow@users.noreply.github.com>
2025-08-05 10:44:42 +0100

    fix: wall collisions and player position in minimap

--- src/renderer/calc_movement.c
+++ src/renderer/calc_movement.c
@@ -6,93 +6,94 @@
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 17:40:43 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/08/04 23:32:06 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/08/05 10:36:33 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/render.h"
 
-static void	move_forward(t_raycaster *rc, unsigned int *last_move);
-static void	move_left(t_raycaster *rc, unsigned int *last_move);
-static void	move_backwards(t_raycaster *rc, unsigned int *last_move);
-static void	move_right(t_raycaster *rc, unsigned int *last_move);
+static void	move_forward(t_raycaster *rc, unsigned int *last_move,
+				double step);
+static void	move_left(t_raycaster *rc, unsigned int *last_move, double step);
+static void	move_backwards(t_raycaster *rc, unsigned int *last_move,
+				double step);
+static void	move_right(t_raycaster *rc, unsigned int *last_move, double step);
 
 void	calc_player_movement(t_raycaster *rc)
 {
 	unsigned int		curr_time;
 	static unsigned int	last_move = 0;
+	double				step;
 
+	step = 0.066;
 	get_time_ms(&curr_time);
 	if (curr_time - last_move > 30)
 	{
 		if (rc->key_state[0] == 1)
-			move_forward(rc, &last_move);
+			move_forward(rc, &last_move, step);
 		if (rc->key_state[1] == 1)
-			move_left(rc, &last_move);
+			move_left(rc, &last_move, step);
 		if (rc->key_state[2] == 1)
-			move_backwards(rc, &last_move);
+			move_backwards(rc, &last_move, step);
 		if (rc->key_state[3] == 1)
-			move_right(rc, &last_move);
+			move_right(rc, &last_move, step);
 	}
 }
 
 /* Player movement helper functions (movement uses direction vector) */
-static void	move_forward(t_raycaster *rc, unsigned int *last_move)
+static void	move_forward(t_raycaster *rc, unsigned int *last_move, double step)
 {
-	int		y;
-	int		x;
+	double	y;
+	double	x;
 
-	y = round(rc->cam->pos_y + rc->cam->dir_y / 15);
-	x = round(rc->cam->pos_x + rc->cam->dir_x / 15);
-	if (rc->world->map[y][x] != '1')
-	{
-		rc->cam->pos_y += rc->cam->dir_y / 15;
-		rc->cam->pos_x += rc->cam->dir_x / 15;
-		get_time_ms(last_move);
-	}
+	y = rc->cam->pos_y + rc->cam->dir_y * step;
+	x = rc->cam->pos_x + rc->cam->dir_x * step;
+	if (rc->world->map[(int)y][(int)rc->cam->pos_x] != '1')
+		rc->cam->pos_y = y;
+	if (rc->world->map[(int)rc->cam->pos_y][(int)x] != '1')
+		rc->cam->pos_x = x;
+	get_time_ms(last_move);
 }
 
-static void	move_left(t_raycaster *rc, unsigned int *last_move)
+static void	move_left(t_raycaster *rc, unsigned int *last_move, double step)
 {
-	int		y;
-	int		x;
+	double	y;
+	double	x;
 
-	y = round(rc->cam->pos_y - rc->cam->dir_x / 15);
-	x = round(rc->cam->pos_x + rc->cam->dir_y / 15);
-	if (rc->world->map[y][x] != '1')
-	{
-		rc->cam->pos_y -= rc->cam->dir_x / 15;
-		rc->cam->pos_x += rc->cam->dir_y / 15;
-		get_time_ms(last_move);
-	}
+	y = rc->cam->pos_y - rc->cam->dir_x * step;
+	x = rc->cam->pos_x + rc->cam->dir_y * step;
+	if (rc->world->map[(int)y][(int)rc->cam->pos_x] != '1')
+		rc->cam->pos_y = y;
+	if (rc->world->map[(int)rc->cam->pos_y][(int)x] != '1')
+		rc->cam->pos_x = x;
+	get_time_ms(last_move);
 }
 
-static void	move_backwards(t_raycaster *rc, unsigned int *last_move)
+static void	move_backwards(t_raycaster *rc, unsigned int *last_move,
+	double step)
 {
-	int		y;
-	int		x;
+	double	y;
+	double	x;
 
-	y = round(rc->cam->pos_y - rc->cam->dir_y / 15);
-	x = round(rc->cam->pos_x - rc->cam->dir_x / 15);
-	if (rc->world->map[y][x] != '1')
-	{
-		rc->cam->pos_y -= rc->cam->dir_y / 15;
-		rc->cam->pos_x -= rc->cam->dir_x / 15;
-		get_time_ms(last_move);
-	}
+	y = rc->cam->pos_y - rc->cam->dir_y * step;
+	x = rc->cam->pos_x - rc->cam->dir_x * step;
+	if (rc->world->map[(int)y][(int)rc->cam->pos_x] != '1')
+		rc->cam->pos_y = y;
+	if (rc->world->map[(int)rc->cam->pos_y][(int)x] != '1')
+		rc->cam->pos_x = x;
+	get_time_ms(last_move);
 }
 
-static void	move_right(t_raycaster *rc, unsigned int *last_move)
+static void	move_right(t_raycaster *rc, unsigned int *last_move, double step)
 {
-	int		y;
-	int		x;
+	double	y;
+	double	x;
 
-	y = round(rc->cam->pos_y + rc->cam->dir_x / 15);
-	x = round(rc->cam->pos_x - rc->cam->dir_y / 15);
-	if (rc->world->map[y][x] != '1')
-	{
-		rc->cam->pos_y += rc->cam->dir_x / 15;
-		rc->cam->pos_x -= rc->cam->dir_y / 15;
-		get_time_ms(last_move);
-	}
+	y = rc->cam->pos_y + rc->cam->dir_x * step;
+	x = rc->cam->pos_x - rc->cam->dir_y * step;
+	if (rc->world->map[(int)y][(int)rc->cam->pos_x] != '1')
+		rc->cam->pos_y = y;
+	if (rc->world->map[(int)rc->cam->pos_y][(int)x] != '1')
+		rc->cam->pos_x = x;
+	get_time_ms(last_move);
 }
--- src/renderer/minimap_player.c
+++ src/renderer/minimap_player.c
@@ -6,7 +6,7 @@
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 13:30:54 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/07/29 17:55:23 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/08/05 10:43:02 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,9 @@ void	draw_player(t_raycaster *rc, float spacing)
 		x = -dx;
 		while (x <= dx)
 		{
-			pixel_put(rc->img, spacing * (rc->cam->pos_x + 0.5) + x
+			pixel_put(rc->img, spacing * rc->cam->pos_x + x
 				+ (WIN_W - rc->world->map_wid * spacing),
-				spacing * (rc->cam->pos_y + 0.5) + y
+				spacing * rc->cam->pos_y + y
 				+ (WIN_H - rc->world->map_len * spacing), YELLOW);
 			x++;
 		}
@@ -58,8 +58,8 @@ static void	draw_gaze(t_raycaster *rc, float spacing)
 	dda.x_inc = dda.dx / dda.step;
 	dda.y_inc = dda.dy / dda.step;
 	i = 0;
-	dda.x1 = spacing * (rc->cam->pos_x + 0.5);
-	dda.y1 = spacing * (rc->cam->pos_y + 0.5);
+	dda.x1 = spacing * rc->cam->pos_x;
+	dda.y1 = spacing * rc->cam->pos_y;
 	while (i <= dda.step)
 	{
 		pixel_put(rc->img, dda.x1 + dda.x_ofs, dda.y1 + dda.y_ofs, MAGENTA);
